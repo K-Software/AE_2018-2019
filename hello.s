06650ac579aab0459685ef19d7e39d6738c19561
Simone Cappabianca <s.cappabianca@gmail.com>
2019-03-11 00:42:11 +0100

    Code review

--- hello.s
+++ hello.s
@@ -12,17 +12,17 @@ buffer: .space 256
 
 main:   li $v0,4        # stampa il prompt
         la $a0,prompt
-        syscall         
+        syscall
 
         li $v0,8        # legge un nome
         la $a0,buffer
         li $a1,256
-        syscall         
+        syscall
 
         li $v0,4        # stampa "ciao " + nome
         la $a0,msg
-        syscall        
+        syscall
 
         li $v0, 10      # termina programma
-        syscall         
+        syscall
 
